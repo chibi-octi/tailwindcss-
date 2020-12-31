09b7b95d759b4d380a058522b63b8af4ff45782b
Declan Talbert <declan_at@hotmail.com>
2020-12-31 18:46:28 +0000

    Installed tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen p-20 bg-blue-500">
+      <div className="bg-white p-4 rounded-t shadow-2x1">
+        This is a card
+      </div>
+      
     </div>
   );
 }
